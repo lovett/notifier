4afb328647fdf26c98d99b43aad4340ad22b094a
Bill Lovett <bill@ilovett.com>
2015-08-08 20:57:42 -0400

    Add project-local customizations for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-mode
+  (flycheck-disabled-checkers javascript-jshint))
+ (json-mode
+  (js-indent-level . 2)))
