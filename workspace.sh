7d94d59aa1fba79cf7d019d53ce6918181f8a772
Bill Lovett <bill@ilovett.com>
2017-01-31 23:48:58 -0500

    Add a workspace script

---
+++ workspace.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -u
+
+PROJECT_ROOT=$(pwd)
+PROJECT_NAME=$(basename "$PROJECT_ROOT")
+
+STARTPAGE="."
+
+LOG=notifier.log
+
+# Start the server if not already running
+tmux start-server 2> /dev/null
+
+# Connect to a session or create a new one
+tmux attach-session -d -t "$PROJECT_NAME" || {
+    echo "Creating a new session"
+
+    ## 0: Editor
+    tmux new-session -d -s "$PROJECT_NAME" bash
+    tmux send-keys -t "$PROJECT_NAME" "$EDITOR $STARTPAGE" C-m
+
+    ## 1: Shell
+    tmux new-window -a -t "$PROJECT_NAME" bash
+
+    ## 2: Grunt
+    tmux new-window -a -t "$PROJECT_NAME" -n "grunt" "grunt"
+
+    ## 3: Log
+    rm -f "$LOG"
+    touch "$LOG"
+    tmux new-window -a -n "log" -t "$PROJECT" "npm run-script logtail:dev"
+
+    ## 4: Dev server
+    tmux new-window -a -t "$PROJECT_NAME" -n "devserver" "npm run-script devserver"
+
+    tmux select-window -t "$PROJECT_NAME":0
+
+    tmux attach-session -t "$PROJECT_NAME"
+}
