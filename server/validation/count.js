51c617cf0653fd9bf13ec077244e1c519f1f0732
Bill Lovett <bill@ilovett.com>
2017-04-22 23:44:51 -0400

    Migrate count validator to ES6

--- server/validation/count.js
+++ server/validation/count.js
@@ -1,22 +1,28 @@
+/**
+ * Validate a numeric string as a number between 1 and 100
+ *
+ * @param {request}  req   - Express request object
+ * @param {response} res   - Express resposne object
+ * @param {next}     next  - Express middleware callback
+ * @param {string}   value - The numeric string to validate
+ */
 function count (req, res, next, value) {
-    var count, err;
 
     if (/\D/.test(value) === true) {
-        err = new Error('Invalid count');
+        let err = new Error('Invalid count');
         err.status = 400;
         next(err);
-    } else {
-        count = parseInt(value, 10);
-
-        if (count > 100) {
-            count = 100;
-        } else if (count === 0) {
-            count = 1;
-        }
-
-        req.params.count = count;
-        next();
+        return;
     }
+
+    let count = parseInt(value, 10);
+
+    count = Math.min(count, 100);
+    count = Math.max(count, 1);
+
+    req.params.count = count;
+
+    next();
 }
 
 exports = module.exports = count;
