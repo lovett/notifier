37b6cb70a53b3226d056c924edfe95c27126cd3d
Bill Lovett <bill@ilovett.com>
2017-05-15 21:20:57 -0400

    Use randomly generated values for default username and password
    Instead of defaulting to a predetermined credential, generate random
    values when the configuration doesn't otherwise specify them. This
    reduces the risk of unauthorized access.

--- modules/helpers/create-user.js
+++ modules/helpers/create-user.js
@@ -1,30 +1,54 @@
 'use strict';
 
-let main = (app) => {
-    let password, username;
+let crypto = require('crypto');
+
+function randomString (len) {
+    let randomBytes;
+
+    randomBytes = crypto.randomBytes(Math.ceil(len/2));
+
+    return randomBytes.toString('hex').slice(0,len);
+}
+
+function main (app) {
+    let filter, password, username;
+
+    const DEFAULT_USER_ID = 1;
 
     username = app.locals.config.get('NOTIFIER_DEFAULT_USER');
     password = app.locals.config.get('NOTIFIER_DEFAULT_PASSWORD');
 
-    if (!username || !password) {
-        return false;
+    if (!username) {
+        username = randomString(6);
     }
 
-    /*
-    app.locals.User.findOrCreate({ where: {username: username}}).spread((user, created) => {
+    if (!password) {
+        password = randomString(12);
+    }
 
-        if (created) {
-            user.hashPassword(password, () => {
-                user.save().then(() => {
-                    callback();
-                }, function (err) {
-                    callback(err);
-                });
-            });
+    filter = {
+        where: {
+            id: DEFAULT_USER_ID
         }
-    }, function (err) {
-        callback(err);
-    });*/
-};
+    };
+
+    app.locals.User.findOne(filter).then(user => {
+        if (user) {
+            process.stdout.write('Default user already exists\n');
+
+            return;
+        }
+
+        user = app.locals.User.create({
+            id: DEFAULT_USER_ID,
+            username: username,
+            passwordHash: password
+        });
+
+        process.stdout.write('Created a default user:\n');
+        process.stdout.write(`- Username: ${username}\n`);
+        process.stdout.write(`- Password: ${password}\n`);
+    });
+}
 
 module.exports = exports = main;
