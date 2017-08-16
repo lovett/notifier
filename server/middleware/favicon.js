9cb5ae51dd1bcb15aa471697d628951455d7817d
Bill Lovett <bill@ilovett.com>
2017-08-16 15:48:14 -0400

    Purge js files from server
    They are probably a carryover from earlier work. With ts-node, there
    shouldn't be a need to have the JS and TS files side-by-side.

--- server/middleware/favicon.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var serveFavicon = require("serve-favicon");
-function default_1(public_dir) {
-    return serveFavicon(public_dir + '/favicon/favicon.ico');
-}
-exports.default = default_1;
-//# sourceMappingURL=favicon.js.map
