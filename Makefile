8e91f67c980fc011a0fd2b27624ad95f6767ac39
Bill Lovett <bill@ilovett.com>
2017-09-28 19:08:24 -0400

    Pull dev-environment build steps back into run scripts
    The previous intent was to run everything through Make, and not use
    NPM scripts. This didn't work out well with tmux. Killing the tmux
    session would leave behind any of the processes that Make had started,
    such as the devserver and the webpack watches.
    
    Running a server process isn't really something that Make is geared
    towards. On the other hand, npm scripts are not able to express
    dependencies as well as Make can.
    
    Instead, use both: scripts for managing long-running processes, but
    not shell-related tasks. Make targets for shell-reltaed tasks, but not
    long-running processes.

--- Makefile
+++ Makefile
@@ -1,24 +1,27 @@
 .PHONY: dummy
 
-all: app worker
+build: app worker
 
 app: dummy
-	NODE_ENV=$(BUILD) npx webpack --config webpack-app.ts
+	npm run build:app
 
-appdev: BUILD=dev
-appdev: app
-
-devserver: dummy
-	NODE_ENV=dev npx nodemon server
-
-onemessage: dummy
-	clients/send-notification -s $(NOTIFIER_DEV) -p 0 -t test -e "5 hours" -u "http://example.com"
-
-livereload: dummy
-	npx livereload public -p 35740 -d -u 1
+hooks: dummy
+	cp hooks/* .git/hooks/
 
 worker: dummy
-	NODE_ENV=$(BUILD) npx webpack --config webpack-worker.ts
+	npm run build:worker
 
-workerdev: BUILD=dev
-workerdev: worker
+onemessage: dummy
+	clients/send-notification -s $(NOTIFIER_DEV) -t test -e "5 hours" -u "http://example.com" -l "onemessage-test"
+
+retract: dummy
+	clients/send-notification -s $(NOTIFIER_DEV) -c -l test
+
+resetdb: dummy
+	rm notifier.sqlite
+	touch server/server.ts
+
+# "test:server": "mocha --bail --reporter min test",
+# "test:client": "karma start app/test/karma.conf.js --single-run",
+# "coverage:server": "istanbul cover --dir coverage/server _mocha -- -R min test",
+# "coverage:client": "echo 'Nope. Client coverage is automatically written by test:client'",
--- workspace.sh
+++ workspace.sh
@@ -6,8 +6,6 @@ set -u
 PROJECT_ROOT=$(pwd)
 PROJECT_NAME=$(basename "$PROJECT_ROOT")
 
-STARTPAGE="."
-
 LOG=notifier.log
 
 # Start the server if not already running
@@ -19,28 +17,27 @@ tmux attach-session -d -t "$PROJECT_NAME" || {
 
     ## 0: Editor
     tmux new-session -d -s "$PROJECT_NAME" bash
-    tmux send-keys -t "$PROJECT_NAME" "$EDITOR $STARTPAGE" C-m
+    tmux send-keys -t "$PROJECT_NAME" "$EDITOR $PROJECT_ROOT" C-m
 
     ## 1: Shell
     tmux new-window -a -t "$PROJECT_NAME" bash
 
     ## 2: Webpack, app
-    tmux new-window -a -t "$PROJECT_NAME" -n "app" "make appdev"
+    tmux new-window -a -t "$PROJECT_NAME" -n "app" "NODE_ENV=dev npm run build:app"
 
     ## 3: Webpack, worker
-    tmux new-window -a -t "$PROJECT_NAME" -n "worker" "make workerdev"
+    tmux new-window -a -t "$PROJECT_NAME" -n "worker" "NODE_ENV=dev npm run build:worker"
 
     ## 4: Livereload
-    tmux new-window -a -t "$PROJECT_NAME" -n "livereload" "make livereload"
+    tmux new-window -a -t "$PROJECT_NAME" -n "watch" "NODE_ENV=dev npm run watch"
 
     ## 5: Dev server
-    tmux new-window -a -t "$PROJECT_NAME" -n "devserver" "make devserver"
+    tmux new-window -a -t "$PROJECT_NAME" -n "devserver" "npm run devserver"
 
     ## 6: Log
     rm -f "$LOG"
     touch "$LOG" 2> /dev/null
-    tmux new-window -a -n "log" -t "$PROJECT_NAME" "tail -f notifier.log"
-
+    tmux new-window -a -t "$PROJECT_NAME" -n "log" "tail -f $LOG"
 
     tmux select-window -t "$PROJECT_NAME":0
 
