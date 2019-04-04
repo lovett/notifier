9b7926afa54435809ca5f6d7cf25c87fa7808148
Bill Lovett <bill@ilovett.com>
2019-04-03 22:29:45 -0400

    Drop unused reference to websocket port
    An earlier version of the application used this to tell the
    client-side application where to open a websocket
    connection. Websockets are no longer in use.
    
    Also simplifies how node environment is determined to improve brevity.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,20 +3,20 @@ import * as express from 'express';
 const router = express.Router();
 
 /**
- * The application homepage
+ * The homepage is an HTML shell that loads the client-side
+ * application, which in turn decides which screen to display.
  */
 router.get('/', (req: express.Request, res: express.Response) => {
     const config = req.app.locals.config;
 
     let title = 'Notifier';
-    if (req.app.get('env') && req.app.get('env') !== 'production') {
+    if (req.app.get('env') !== 'production') {
         title += ' ' + req.app.get('env');
     }
 
     res.render('index', {
         base_href: config.get('NOTIFIER_BASE_URL'),
         title,
-        websocket_port: config.get('NOTIFIER_WEBSOCKET_PORT'),
     });
 });
 
