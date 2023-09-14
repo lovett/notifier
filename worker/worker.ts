ca6617b89f40f3a0678315a4f6607dea81499ad4
Bill Lovett <bill@ilovett.com>
2023-09-14 18:19:07 -0400

    Let Firefox on Android use EventSource instead of polling.
    It seems to work now, so the polling workaround is no longer necessary.

--- worker/worker.ts
+++ worker/worker.ts
@@ -2,30 +2,11 @@ import {Command, Event} from './postmessage';
 
 let eventSource: EventSource;
 let reconnectTimer: number;
-let pollingTimer: number;
-
-const isAndroid = self.navigator.userAgent.indexOf('Android') > -1;
-const isFirefox = self.navigator.userAgent.indexOf('Firefox') > -1;
-
-/**
- * There is a long-standing problem with worker-based EventSource
- * connections on Firefox for Android that causes the browser to crash
- * when the page is reloaded or closed. Fetch-based polling is used as
- * a workaround.
- */
-const usePolling = (isAndroid && isFirefox);
-const pollingInterval = 5000;
 
 /**
  * Open a persistent connection to the server for real-time push.
  */
 function connect() {
-    if (usePolling) {
-        self.postMessage(Event.connected);
-        pollingTimer = setInterval(poll, pollingInterval);
-        return;
-    }
-
     eventSource = new EventSource('push');
 
     eventSource.onopen = () => {
@@ -46,24 +27,10 @@ function connect() {
     };
 }
 
-async function poll() {
-    const since = Date.now() - pollingInterval;
-    const response = await fetch(
-        `archive?since=${since}`,
-        { credentials: 'same-origin' },
-    );
-    const json = await response.json();
-    json.forEach((message: unknown) => {
-        self.postMessage(message);
-    });
-}
-
 /**
- * Stop listening (or polling) for new messages.
+ * Stop listening for new messages.
  */
 function disconnect() {
-    clearTimeout(pollingTimer);
-
     if (eventSource) {
         eventSource.close();
     }
