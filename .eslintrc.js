8b7b51319d50c3eb035118c9c2863ed801740b6f
Bill Lovett <bill@ilovett.com>
2024-04-05 18:56:28 -0400

    Update eslint config to match current docs for typescript support.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
