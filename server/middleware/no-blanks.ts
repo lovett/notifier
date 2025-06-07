6dbdd897e7235907ebc8078665e15aafa301c247
Bill Lovett <bill@ilovett.com>
2025-06-06 21:23:31 -0400

    Adjust no-blanks middleware for body-parser v2.

--- server/middleware/no-blanks.ts
+++ server/middleware/no-blanks.ts
@@ -4,19 +4,21 @@ import { NextFunction, Request, Response } from 'express';
  * Remove leading and trailing whitespace from a parsed request body.
  *
  * If there keys in the body with no value, they are set to null.
+ *
+ * As of body-parser v2, req.body may not start out as an empty object
+ * as it previously did in v1.
  */
 export default (req: Request, _: Response, next: NextFunction): void => {
-    Object.keys(req.body).forEach((key) => {
-        const value = req.body[key];
-        if (typeof value !== 'string') {
-            return;
-        }
+  for (let key in req.body ?? {}) {
+    if (typeof req.body[key] !== 'string') {
+      continue;
+    }
 
-        req.body[key] = value.trim();
+    req.body[key] = req.body[key].trim();
+    if (req.body[key].length === 0) {
+      req.body[key] = null;
+    }
+  }
 
-        if (req.body[key].length === 0) {
-            req.body[key] = null;
-        }
-    });
-    next();
+  next();
 };
