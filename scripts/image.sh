90dd0a52ed3ef3aa7a7c06a335c2148d381bf0da
Bill Lovett <bill@ilovett.com>
2026-07-09 12:39:34 -0400

    Add app version as image metdata for use during CI tag creation.

--- scripts/image.sh
+++ scripts/image.sh
@@ -10,18 +10,18 @@ rm -f server/public/*.html server/public/*.js server/public*.css
 
 $BUN build ui/index.html ui/worker.ts --outdir server/public
 
-date +'%Y.%m.%d+' > "$VERSION_FILE"
-truncate -s-1 "$VERSION_FILE"
-git rev-parse --short=5 HEAD >> "$VERSION_FILE"
+APP_VERSION="$(date +'%Y.%m.%d')+$(git rev-parse --short=5 HEAD)"
+echo "$APP_VERSION" > "$VERSION_FILE"
 
 podman build -t notifier \
        --inherit-labels=false \
-	   --label=org.opencontainers.image.created="$(date --rfc-3339='seconds')" \
-	   --label=org.opencontainers.image.description="$(jq -r .description package.json)" \
-	   --label=org.opencontainers.image.revision="$(git rev-parse HEAD)" \
-	   --label=org.opencontainers.image.title="$(jq -r .name package.json)" \
-	   --label=org.opencontainers.image.url="$(jq -r .homepage package.json)" \
-	   .
+       --label=org.opencontainers.image.created="$(date --rfc-3339='seconds')" \
+       --label=org.opencontainers.image.description="$(jq -r .description package.json)" \
+       --label=org.opencontainers.image.revision="$(git rev-parse HEAD)" \
+       --label=org.opencontainers.image.title="$(jq -r .name package.json)" \
+       --label=org.opencontainers.image.url="$(jq -r .homepage package.json)" \
+       --label=notifier.version="$APP_VERSION" \
+       .
 
 podman image prune -f
 git checkout -q "$VERSION_FILE"
